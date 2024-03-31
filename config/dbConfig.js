c55337b766c69abe0d06ff3d26962abb5ca72a3a
Raghav GC <raghav.gc@worldlink.com.np>
2024-04-01 00:24:04 +0545

    cicd test

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
-  host: process.env.HOST,
-  dialect: "mysql",
-  operatorsAliases: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: localhost,
+    dialect: "mysql",
+    operatorsAliases: false,
+  }
+);
 
 const db = {};
 
