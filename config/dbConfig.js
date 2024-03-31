0e3d4672faef0a6f685ad53587ed7b31ff627827
Raghav GC <raghav.gc@worldlink.com.np>
2024-04-01 00:28:32 +0545

    cicd test

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,31 +1,23 @@
-require("dotenv").config();
-
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.USER,
-  process.env.PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    operatorsAliases: false,
-  }
-);
+const sequelize = new Sequelize("form_builder", "root", "R@ghavmessi10", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.DataTypes = DataTypes
+db.DataTypes = DataTypes;
 
 const User = require("../models/User")(sequelize, DataTypes);
 const Org = require("../models/Organization")(sequelize, DataTypes);
-const Form = require('../models/Form')(sequelize, DataTypes);
-const FormField = require('../models/FormField')(sequelize, DataTypes);
+const Form = require("../models/Form")(sequelize, DataTypes);
+const FormField = require("../models/FormField")(sequelize, DataTypes);
 const Submission = require("../models/Submission")(sequelize, DataTypes);
 
-Form.hasMany(FormField, { foreignKey: 'form_id' });
+Form.hasMany(FormField, { foreignKey: "form_id" });
 FormField.belongsTo(Form, { foreignKey: "form_id" });
 
 Form.hasMany(Submission, { foreignKey: "form_id" });
